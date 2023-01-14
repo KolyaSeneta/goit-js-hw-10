ea74a082557c00fb0fa7ce5b1d6a1d2913340b46
KolyaSeneta <114915582+KolyaSeneta@users.noreply.github.com>
2023-01-14 15:32:27 +0200

    finish

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { countryCardTemplate, countryListTemplate } from './js/template';
+var debounce = require('lodash.debounce');
+import { refs } from './js/refs';
 
 const DEBOUNCE_DELAY = 300;
+refs.searchBox.addEventListener(
+  'input',
+  debounce(onInputCountry, DEBOUNCE_DELAY)
+);
+
+function onInputCountry() {
+  const countryName = refs.searchBox.value.trim();
+  if (countryName === '') {
+    refs.countryInfo.innerHTML = '';
+    refs.countryList.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(countryName)
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.countryInfo.innerHTML = '';
+        refs.countryList.innerHTML = '';
+        return;
+      }
+
+      if (countries.length <= 10 && countries.length > 1) {
+        const listMarkup = countries.map(country =>
+          countryListTemplate(country)
+        );
+        refs.countryList.innerHTML = listMarkup.join('');
+        refs.countryInfo.innerHTML = '';
+      }
+
+      if (countries.length === 1) {
+        const markup = countries.map(country => countryCardTemplate(country));
+        refs.countryInfo.innerHTML = markup.join('');
+        refs.countryList.innerHTML = '';
+      }
+    })
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryInfo.innerHTML = '';
+      refs.countryList.innerHTML = '';
+      return error;
+    });
+}
